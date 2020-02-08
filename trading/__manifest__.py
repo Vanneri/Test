618987228bd783b01f6c1e4b1e1fc5f347aa8e27
Vanneri <vanneri.odoodev@gmail.com>
2020-02-09 01:28:31 +0530

    Structure Changes

--- trading/__manifest__.py
+++ trading/__manifest__.py
@@ -1,26 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "trading",
+    'name': "Trading Module",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+        Introduces diffrent type of trading concepts 
+        """,
+    'author': "Vanneri",
+    'website': "https://github.com/Vanneri/Test/tree/12.0",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Extra Tools',
+    'version': '2.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['sale','purchase','account'],
 
     # always loaded
     'data': [
